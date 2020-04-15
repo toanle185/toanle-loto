61521dabb6e3ac164d3164ba3084577c17b7b3c5
Toan Le <quytoan@trycz.vn>
2020-04-15 17:03:25 +0700

    Updates

---
+++ precache-manifest.29bd59058075c62c37a41ca416b7ec06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259874bbb31dc42104103adf8ef9079a",
+    "url": "/toanle-loto/index.html"
+  },
+  {
+    "revision": "8c32a33a4028db876a43",
+    "url": "/toanle-loto/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "a94f53f6dcb746def713",
+    "url": "/toanle-loto/static/css/main.9e6be6ce.chunk.css"
+  },
+  {
+    "revision": "8c32a33a4028db876a43",
+    "url": "/toanle-loto/static/js/2.dd2625c1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/toanle-loto/static/js/2.dd2625c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a94f53f6dcb746def713",
+    "url": "/toanle-loto/static/js/main.736ffa76.chunk.js"
+  },
+  {
+    "revision": "e33de4099bd73bb2167c",
+    "url": "/toanle-loto/static/js/runtime-main.52a79ab4.js"
+  }
+]);
